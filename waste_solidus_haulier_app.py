18d83bf0e0c32f07a7d381dbd69e7a8c0678da69
cdenton-apps <connordenton@hotmail.co.uk>
2025-12-16 16:22:15 +0000

    Update waste_solidus_haulier_app.py

--- waste_solidus_haulier_app.py
+++ waste_solidus_haulier_app.py
@@ -43,7 +43,7 @@ with col_text:
     )
     st.markdown(
         """
-        **V1.3.8**
+        **V2.0.0**
         
         Enter a UK postcode area, select a service type (Economy or Next Day),  
         specify the number of pallets, and apply fuel surcharges and optional extras:
